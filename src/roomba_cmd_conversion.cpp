5095fd59e37ae6b3a23fb53e5fb1c3aba4ea4e0c
bakejeyner <bakejeyner@gmail.com>
2017-02-20 16:24:55 -0700

    finished implementing and testing the navigation stack

--- src/roomba_cmd_conversion.cpp
+++ src/roomba_cmd_conversion.cpp
@@ -24,8 +24,8 @@ void Cmd_Conversion::velCallback(const geometry_msgs::Twist::ConstPtr& msg)
   //debug message
   ROS_INFO("\nlinear x: %f\nlinear y: %f\nlinear z: %f\nangular x: %f\nangular y: %f\nangular z: %f\n\n", msg->linear.x, msg->linear.y, msg->linear.z, msg->angular.x, msg->angular.y, msg->angular.z);
 
-  double angularThreshold = 0.1;
-  double linearThreshold = 0.1;
+  double angularThreshold = 0.05;
+  double linearThreshold = 0.05;
   
   double vyaw = msg->angular.z;
   double vx = msg->linear.x;
@@ -34,17 +34,21 @@ void Cmd_Conversion::velCallback(const geometry_msgs::Twist::ConstPtr& msg)
   
   //debug variables
   ROS_INFO("\nvyaw: %f\nvx: %f\nvy: %f\nv: %f\n\n", vyaw, vx, vy, v);
+  ROS_INFO("abs(vyaw): %f\n", std::abs(vyaw));
+  ROS_INFO("abs(v): %f\n", std::abs(v));
    
   //messages
   std_msgs::Int8 msg_roomba;
   
   //handle rotation first
-  if ( abs(vyaw) >= angularThreshold )
+  if ( std::abs(vyaw) >= angularThreshold )
   {
+    ROS_INFO("Inside rotation publish thingy");
   
     //ccw
     if (vyaw > 0)
     {
+      ROS_INFO("Inside positive rotation publish thingy");
       msg_roomba.data = 2;
       pub_cmd_roomba->publish(msg_roomba);
       return;
@@ -54,6 +58,7 @@ void Cmd_Conversion::velCallback(const geometry_msgs::Twist::ConstPtr& msg)
     //cw
     else
     {
+      ROS_INFO("Inside negative rotation publish thingy");
       msg_roomba.data = 3;
       pub_cmd_roomba->publish(msg_roomba);
       return;
@@ -62,7 +67,7 @@ void Cmd_Conversion::velCallback(const geometry_msgs::Twist::ConstPtr& msg)
   
   
   //handle velocity if no rotation
-  else if ( abs(v) >= linearThreshold )
+  else if ( std::abs(v) >= linearThreshold )
   {
     ROS_INFO("Inside velocity publish thingy");
     msg_roomba.data = 1;
@@ -74,6 +79,7 @@ void Cmd_Conversion::velCallback(const geometry_msgs::Twist::ConstPtr& msg)
   //if no rotation or velocity set done flag
   else
   {
+    ROS_INFO("No rotation published");
     done = true;
     return;
   }
@@ -86,15 +92,10 @@ bool Cmd_Conversion::convertCallback(roomba_tf::convert::Request& req, roomba_tf
   
   ros::Rate r(2);
   
-  sub_cmd_vel = n->subscribe("/cmd_vel", 100, &Cmd_Conversion::velCallback, this);
-  
-  ROS_INFO("In convertCallback");
-  ROS_INFO(done ? "done true" : "done false");
+  sub_cmd_vel = n->subscribe("/cmd_vel", 10, &Cmd_Conversion::velCallback, this);
   
   while( n->ok() )
-  {
-    ROS_INFO("In convertCallback while loop");
-    
+  { 
     ros::spinOnce();
     
     //if finished, exit
@@ -125,7 +126,7 @@ int main(int argc, char **argv)
   Cmd_Conversion cmd;
   cmd.n = &n;
   
-  ros::Publisher pub_cmd_roomba = n.advertise<std_msgs::Int8>("cmd_roomba", 10);
+  ros::Publisher pub_cmd_roomba = n.advertise<std_msgs::Int8>("/cmd_roomba", 10);
   cmd.pub_cmd_roomba = &pub_cmd_roomba;
   
   ros::ServiceServer service = n.advertiseService("/controller_conversion", &Cmd_Conversion::convertCallback, &cmd);
